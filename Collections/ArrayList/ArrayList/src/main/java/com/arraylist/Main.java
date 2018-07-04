6dba99c762e065cf0692d33befc840fdc0a852b6
MKerch <kerchmaksim@gmail.com>
2018-07-04 16:02:49 +0300

    ArrayList was added

---
+++ Collections/ArrayList/ArrayList/src/main/java/com/arraylist/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arraylist;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kerch
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ArrayList<String> obj = new ArrayList<>();
+
+        /*This is how elements should be added to the array list*/
+        obj.add("Ajeet");
+        obj.add("Harry");
+        obj.add("Chaitanya");
+        obj.add("Steve");
+        obj.add("Anuj");
+
+        /* Displaying array list elements */
+        System.out.println("Currently the array list has following elements:" + obj);
+
+        /*Add element at the given index*/
+        obj.add(0, "Rahul");
+        obj.add(1, "Justin");
+
+        /*Remove elements from array list like this*/
+        obj.remove("Chaitanya");
+        obj.remove("Harry");
+
+        System.out.println("Current array list is:" + obj);
+
+        /*Remove element from the given index*/
+        obj.remove(1);
+
+        System.out.println("Current array list is:" + obj);
+    }
+
+}
